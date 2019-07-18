1591ed6ba107526cc3b5d78ce0d247cf66424258
DimitriKem <dimitrikemaikin@gmail.com>
2019-07-18 12:51:17 +0300

    3components + dataPicker

--- src/app/index.js
+++ src/app/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import styles from './app.module.css';
 
 import 'babel-polyfill';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 import Titles from "./../components/Titles";
 import Form from "./../components/Form";
 import Weather from "./../components/Weather";
 
 const API_KEY = "d05f1216f3f9ed37047e4cb2683adc49";
 
+Date.prototype.getUnixTime = function() { return this.getTime()/1000|0 };
 class App extends React.Component {
   state = {
+    data: new Date(),
     temperature: undefined,
     city: undefined,
     country: undefined,
@@ -17,10 +22,21 @@ class App extends React.Component {
     description: undefined,
     error: undefined
   }
+
+  handleChangeData = (data) => {
+    this.setState({
+      ...this.state,
+      data
+    })
+  }
+
+  
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
+    //const parsedUnixTime = this.data.getUnixTime;
+    //http://history.openweathermap.org/data/2.5/history/city?q={city ID},{country code}&type=hour&start={start}&cnt={cnt}
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
     if (city && country) {
@@ -46,17 +62,22 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        
         <div className="wrapper">
           <div className="main">
             <div className="container">
               <div className="row">
                 <div className="col-xs-5 title-container">
                   <Titles />
+                  <DatePicker
+                      selected={this.state.data}
+                      onChange={this.handleChangeData}
+                    />
                 </div>
                 <div className="col-xs-7 form-container">
                   <Form getWeather={this.getWeather} />
-                  <Weather 
-                    temperature={this.state.temperature} 
+                  <Weather
+                    temperature={this.state.temperature}
                     humidity={this.state.humidity}
                     city={this.state.city}
                     country={this.state.country}
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,22 +3,22 @@ import React from "react";
 const Weather = props => (
 	<div className="weather__info">
 	 {	
-	 	props.city && props.country && <p className="weather__key"> Location: 
+	 	props.city && props.country && <p className="weather__key"> Место: 
 	 		<span className="weather__value"> { props.city }, { props.country }</span>
 	 	</p> 
 	 }
 	 { 	
-	 	props.temperature && <p className="weather__key"> Temperature: 
+	 	props.temperature && <p className="weather__key"> Температура: 
 	 		<span className="weather__value"> { props.temperature }	</span>
 	 	</p> 
 	 }
 	 { 	
-	 	props.humidity && <p className="weather__key"> Humidity: 
+	 	props.humidity && <p className="weather__key"> Влажность: 
 	 		<span className="weather__value"> { props.humidity } </span>
 	 	</p> 
 	 }
 	 { 	
-	 	props.description && <p className="weather__key"> Conditions: 
+	 	props.description && <p className="weather__key"> Описание: 
 	 		<span className="weather__value"> { props.description } </span>
 	 </p> 
 	 }
